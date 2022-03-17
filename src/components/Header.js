4bbcad1458516a46bd13b5b373f28101659e6b2f
Jacks Bruno Moran Prado <jacks.prado@grupopereira.local>
2022-03-17 12:28:09 -0400

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { StyleSheet, View, Text, Platform, Image } from 'react-native'
+
+export default props => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.rowContainer}>
+        <Image source={require('../assets/imgs/icon.png')} style={styles.image} />
+        <Text style={styles.title}>Instagram</Text>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Platform.OS === 'ios' ? 20 : 0,
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: '#bbb'
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  image: {
+    height: 30,
+    width: 30,
+    resizeMode: 'contain'
+  },
+  title: {
+    color: '#000',
+    fontFamily: 'shelter',
+    height: 30,
+    fontSize: 28
+  }
+})
